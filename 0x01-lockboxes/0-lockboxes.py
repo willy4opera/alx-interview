69ae48d9a733d8bfd1f14d8c521b9e1eb5b68415
willyopera <willy4opera@gmail.com>
2024-03-14 18:35:54 -0400

    Completed the interview challenge for check boxed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,18 +4,18 @@
 
 
 def canUnlockAll(boxes):
-    '''Checks that all the boxes in a list of boxes containing keys
+    '''Checks if all the boxes in a list of boxes containing the keys
     (indices) to other boxes can be unlocked given that the first
     box is unlocked.
     '''
-    num = len(boxes) ''Gets the length of box ''
-    viewed = set([0])
-    unviewed_boxes = set(boxes[0]).difference(set([0]))
-    while len(unviewed_boxes) > 0:
-        Idbox = unviewed_boxes.pop()
-        if not Idbox or Idbox >= num or Idbox < 0:
+    num = len(boxes)
+    viewed_box = set([0])
+    unviewed = set(boxes[0]).difference(set([0]))
+    while len(unviewed) > 0:
+        IdBox = unviewed.pop()
+        if not IdBox or IdBox >= num or IdBox < 0:
             continue
-        if Idbox not in viewed:
-            unview_boxes = unviewed_boxes.union(boxes[Idbox])
-            viewed.add(Idbox)
-    return n == len(viewed)
+        if IdBox not in viewed_box:
+            unviewed = unviewed.union(boxes[IdBox])
+            viewed_box.add(IdBox)
+    return num == len(viewed_box)
