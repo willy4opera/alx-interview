3e08c4e9f153fc6cdf4c086ae00bfab9834e95ef
willyopera <willy4opera@gmail.com>
2024-03-21 21:08:53 -0400

    Added the 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+'''The python minimum operations coding challenge.
+'''
+
+
+def minOperations(n):
+    '''Calculated the least number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
+        return 0
+    option_c = 0
+    clpbd = 0
+    completed = 1
+    # print('H', end='')
+    while completed < n:
+        if clpbd == 0:
+            # init (the first copy all and paste)
+            clpbd = done
+            completed += clpbd
+            option_c += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - completed > 0 and (n - done) % completed == 0:
+            # copy all and paste
+            clpbd = done
+            completed += clpbd
+            option_c += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clpbd > 0:
+            # paste
+            completed += clpbd
+            option_c += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return option_c
