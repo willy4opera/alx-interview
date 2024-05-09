e5bc85a1a52052af0b87e68ccdcabbe8056abe0f
willyopera <willy4opera@gmail.com>
2024-05-09 02:57:51 -0400

    Added the Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+'''Computing the island perimeter module.
+'''
+
+
+def island_perimeter(grid):
+    '''Here, we computed the perimeter of an island with no lakes.
+    '''
+    perimeter = 0
+    if type(grid) != list:
+        return 0
+    numx = len(grid)
+    for x, row in enumerate(grid):
+        numy = len(row)
+        for y, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                x == 0 or (len(grid[x - 1]) > y and grid[x - 1][y] == 0),
+                y == numy - 1 or (numy > y + 1 and row[y + 1] == 0),
+                x == numx - 1 or (len(grid[x + 1]) > y and grid[x + 1][y] == 0),
+                y == 0 or row[y - 1] == 0,
+            )
+            perimeter += sum(edges)
+    return perimeter
